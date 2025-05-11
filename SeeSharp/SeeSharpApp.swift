9c8cebd7312297b1c34f46ba8b4adca74432ee54
hawkings232 <asiankidjoe6@gmail.com>
2025-05-11 10:54:23 -0500

    Initial Commit

---
+++ SeeSharp/SeeSharpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeSharpApp.swift
+//  SeeSharp
+//
+//  Created by Eddie Zhou on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SeeSharpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SeeSharpTests/SeeSharpTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeeSharpTests.swift
+//  SeeSharpTests
+//
+//  Created by Eddie Zhou on 5/11/25.
+//
+
+import Testing
+@testable import SeeSharp
+
+struct SeeSharpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
